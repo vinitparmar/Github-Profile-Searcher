beebc912933ce2260cac2435b1b0d7a4603d6171
vinit parmar <vinitparmar89@gmail.com>
2018-02-14 21:46:42 +0530

    resolved issue

--- src/app/components/profile.component.ts
+++ src/app/components/profile.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { GithubService } from '../services/github.service';
 
 @Component({
-	moduleId: module.id,
+  moduleId: module.id,
   selector: 'profile',
   templateUrl: 'profile.component.html',
 })
